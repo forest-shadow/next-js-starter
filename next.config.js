5a3228b6983cefd791df23dcb886d1dca0784a29
forest-shadow <mamasay@yandex.ru>
2019-07-14 20:34:39 +0300

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,4 @@
+// next.config.js
+const withStylus = require('@zeit/next-stylus')
+
+module.exports = withStylus()
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import '../stylus/styles.styl'
+
+export default class extends React.Component {
+  render() {
+    return <div className='example'>Hello Next.js</div>
+  }
+}
